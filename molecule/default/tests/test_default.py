37552ca5f1bd8fafd1e75fc0526402ffaf6b1d0a
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-07-09 11:39:58 -0400

    Add freeipa-server-dns to the packages to be installed

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,7 +10,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("pkg", ["freeipa-server"])
+@pytest.mark.parametrize("pkg", ["freeipa-server", "freeipa-server-dns"])
 def test_packages(host, pkg):
     """Test that the appropriate packages were installed."""
     assert host.package(pkg).is_installed

568a46cc3485d7cb5c7746a1e67e1ffc157748f7
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-10-04 16:56:23 -0400

    Add a test to make sure the cron job was installed as expected

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -14,3 +14,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_packages(host, pkg):
     """Test that the appropriate packages were installed."""
     assert host.package(pkg).is_installed
+
+
+@pytest.mark.parametrize("f", ["/etc/cron.daily/disable_inactive_users.sh"])
+def test_files(host, f):
+    """Test that the appropriate files were installed."""
+    assert host.file(f).exists
+    assert host.file(f).is_file
+    assert host.file(f).user == "root"
+    assert host.file(f).group == "root"
+    assert host.file(f).mode == 0o500

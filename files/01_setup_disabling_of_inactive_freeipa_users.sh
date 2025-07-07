416c006b74d1ccb396287aa9aa66c9bf5d3722b8
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-07-07 12:29:43 -0400

    Use systemctl enable --now to force timer start

--- files/01_setup_disabling_of_inactive_freeipa_users.sh
+++ files/01_setup_disabling_of_inactive_freeipa_users.sh
@@ -62,8 +62,7 @@ function setup {
   # Enable and start the systemd timer that runs the service that runs
   # the script that disables inactive users.
   systemctl daemon-reload
-  systemctl enable disable-inactive-freeipa-users.timer
-  systemctl start disable-inactive-freeipa-users.timer
+  systemctl enable --now disable-inactive-freeipa-users.timer
 }
 
 if [ $# -ne 0 ]; then

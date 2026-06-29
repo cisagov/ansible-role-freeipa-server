49b821265295b414b036043655efe65dff9a91af
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-06-29 14:49:48 -0400

    Remove reinitialization code
    I haven't been running this code when redeploying anyway.

--- files/02_amend_replication_agreements.sh
+++ files/02_amend_replication_agreements.sh
@@ -55,58 +55,10 @@ function enable_last_successful_auth_replication {
   done
 }
 
-# Re-initialize this replica.  This should be run after the topology
-# segments are altered by the enable_last_successful_auth_replication
-# function so that data is immediately synchronized.
-function reinitialize_replica {
-  # The suffix just needs to be valid, since every topology suffix
-  # with the same name but a different topology suffix should be
-  # otherwise identical.
-  first_suffix="${TOPOLOGY_SUFFIXES[0]}"
-  my_hostname=$(hostnamectl status --static)
-
-  # Find a topology segment between the server where this script is
-  # being run and the server against which we created the replica.
-  # From the name of that segment extract the hostname of the server
-  # against which we created the replica.
-  #
-  # Note that we only print the first segment name that is a match
-  # since any match will work.
-  #
-  # The T in the sed command means "jump if no substitution took
-  # place" which means that the p and q commands will ONLY run if a
-  # substitution took place; otherwise, we proceed to the next line.
-  #
-  # This command may fail if the topology segment we're looking for is
-  # the other way around, i.e., if the server where we're running this
-  # script is the left node in the topology segment.  In that case
-  # segment_name will be empty and we will run a different command.
-  set +o errexit
-  segment_name=$(ipa topologysegment-find "$first_suffix" --pkey-only \
-    --rightnode="$my_hostname" \
-    | sed --quiet "s/^[[:blank:]]*Segment name:[[:blank:]]*\(.*\)$/\1/; T; p; q")
-  set -o errexit
-  if [ -n "$segment_name" ]; then
-    other_hostname=$(sed --quiet "s/^\(.*\)-to-$my_hostname$/\1/p" \
-      <<< "$segment_name")
-  else
-    # There was no match, so the topology segment name must be the
-    # other way around.
-    segment_name=$(ipa topologysegment-find "$first_suffix" --pkey-only \
-      --leftnode="$my_hostname" \
-      | sed --quiet "s/^[[:blank:]]*Segment name:[[:blank:]]*\(.*\)$/\1/; T; p; q")
-    other_hostname=$(sed --quiet "s/^$my_hostname-to-\(.*\)$/\1/p" \
-      <<< "$segment_name")
-  fi
-
-  ipa-replica-manage re-initialize --from="$other_hostname"
-}
-
 # Enable replication of the krblastsuccessfulauth timestamps on all
-# existing topology segments and then reinitialize this replica.
+# existing topology segments.
 function setup {
   enable_last_successful_auth_replication
-  reinitialize_replica
 }
 
 if [ $# -eq 0 ]; then
